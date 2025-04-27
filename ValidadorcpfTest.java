caeab7926e304e059609000444d298f91bf9e8cd
eduardosiilvaa <eduardosiilvaa@example.com>
2025-04-27 12:12:43 -0300

    Primeiro commit - Adicionando ValidadorCpf e testes unitários

---
+++ ValidadorcpfTest.java
@@ -0,0 +1,17 @@
+package com.validarCpf;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidadorCpfTest {
+
+    @Test
+    public void testCpfValido() {
+        assertTrue(ValidadorCpf.validarCpf("12345678901"));
+    }
+
+    @Test
+    public void testCpfInvalido() {
+        assertFalse(ValidadorCpf.validarCpf("123456789"));
+    }
+}
